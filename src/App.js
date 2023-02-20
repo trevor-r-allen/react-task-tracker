5cc120fb499d93a2a74cf5549e81830019a727e9
trevor-r-allen <tallen2699@gmail.com>
2023-02-20 09:54:32 -0700

    front end finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from "./Components/Header"
+import Tasks from './Components/Tasks'
+import AddTask from './Components/AddTask'
+
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Task 1',
+      day: 'Feb 1',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Task 2',
+      day: 'Feb 2',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Task 3',
+      day: 'Feb 3',
+      reminder: false,
+    }
+  ])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks to Show')}
     </div>
   );
 }
